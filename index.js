11438fce16c6e1c10d5c404a374b5461f2318a49
Felipe Hernández Palafox <felipe.sistemas.fhp@gmail.com>
2021-11-04 19:18:54 -0600

    Primer Comit
    Primer Comit

---
+++ index.js
@@ -0,0 +1,39 @@
+function registrar() {
+    const name = document.getElementById("nombre").value;
+    const email = document.getElementById("correo").value;
+    const password = document.getElementById("password").value;
+
+    var emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+    if (!emailRegex.test(email)) {
+      alert("Email incorrecto");
+      return false;
+    }
+
+    const resgistry = {
+      name: name,
+      email: email,
+      password: password
+    };
+
+    console.log(resgistry);
+
+    if (resgistry.email == "felipe.hernandez.palafox@gmail.com"){
+        console.log("Bienvenido Couch");
+    }else {
+        console.log("Bienvenido Ucamper")
+    }
+
+    document.getElementById("informacion").innerHTML = name;
+
+
+}
+
+
+function enterCajaTexto(e) {
+    
+    if (e.keyCode == 13) {
+        registrar();
+    }
+
+}
